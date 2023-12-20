51a5f9f766753def56e94c44a71e97f88ad5dd80
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-12-20 09:50:39 +0700

    add images asset & add data.js

---
+++ src/data.js
@@ -0,0 +1,65 @@
+const portfolio = {
+  header: {
+    IntroProfile: {
+      name: 'Muhammad Iqbal',
+      role: 'MERN Web Developer',
+      excerpt: 'I build exceptional and accessible digital experiences for the web.'
+    },
+    navbar: [
+      { name: 'ABOUT', url: "#about" },
+      { name: 'EXPERIENCE', url: "#experience" },
+      { name: 'PROJECTS', url: "#projects" }
+    ],
+    SocialMedia: [
+      { name: 'Github', url: "https://www.github.com" },
+      { name: 'Instagram', url: "https://www.instagram.com" },
+      { name: 'Twitter', url: "https://www.twitter.com" },
+      { name: 'LinkedIn', url: "https://www.linkedin.com" },
+    ]
+  },
+  main: {
+    experienceSection: {
+      sectionTitle: 'EXPERIENCE',
+      experienceList: [
+        {
+          period: '2018-2019',
+          role: 'Front-End Web Developer',
+          company: 'Gikslab',
+          desc: 'Deliver high-quality, robust production code for a diverse array of projects for clients including Harvard Business School, Everytown for Gun Safety, Pratt Institute, Koala Health, Vanderbilt University, The 19th News, and more. Provide leadership within engineering department through close collaboration, knowledge shares, and mentorship.',
+          technologies: ['MongoDb', 'Express.js', 'React.js', 'Node.js', 'Tailwind', 'Framer Motion']
+        },
+        {
+          period: '2018-2019',
+          role: 'Front-End Web Developer',
+          company: 'Gikslab',
+          desc: 'Deliver high-quality, robust production code for a diverse array of projects for clients including Harvard Business School, Everytown for Gun Safety, Pratt Institute, Koala Health, Vanderbilt University, The 19th News, and more. Provide leadership within engineering department through close collaboration, knowledge shares, and mentorship.',
+          technologies: ['MongoDb', 'Express.js', 'React.js', 'Node.js', 'Tailwind', 'Framer Motion']
+        }
+      ]
+    },
+    projectsSection: {
+      sectionTitle: 'PROJECTS',
+      projectsList: [
+        {
+          period: '2018-2019',
+          role: 'Front-End Web Developer',
+          company: 'Gikslab',
+          desc: 'Deliver high-quality, robust production code for a diverse array of projects for clients including Harvard Business School, Everytown for Gun Safety, Pratt Institute, Koala Health, Vanderbilt University, The 19th News, and more. Provide leadership within engineering department through close collaboration, knowledge shares, and mentorship.',
+          technologies: ['MongoDb', 'Express.js', 'React.js', 'Node.js', 'Tailwind', 'Framer Motion']
+        },
+        {
+          period: '2018-2019',
+          role: 'Front-End Web Developer',
+          company: 'Gikslab',
+          desc: 'Deliver high-quality, robust production code for a diverse array of projects for clients including Harvard Business School, Everytown for Gun Safety, Pratt Institute, Koala Health, Vanderbilt University, The 19th News, and more. Provide leadership within engineering department through close collaboration, knowledge shares, and mentorship.',
+          technologies: ['MongoDb', 'Express.js', 'React.js', 'Node.js', 'Tailwind', 'Framer Motion']
+        }
+      ]
+    },
+  },
+  footer: {
+    copyright: 'Developed & designed by Muhammad Iqbal 🚀 2023.'
+  }
+};
+
+export default portfolio;
