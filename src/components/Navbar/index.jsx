674e84294bb7816a0d9be7a7ef388d2b789be9f3
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-12-20 14:52:40 +0700

    add navLink onClick function

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './navbar.module.css';
 import portfolioData from '../../portfolioData';
 
 const Navbar = () => {
+  const [activeSection, setActiveSection] = useState('about');
+
+  const handleClick = (sectionName) => {
+    setActiveSection(sectionName);
+  };
+  
   const navList = portfolioData.header.navbar;
   return (
     <nav className={styles.navbar}>
       <ul className={styles.navList}>
         {navList.map((navItem) => (
-          <li key={navItem.name}>
-            <a href={navItem.url} className={styles.navLink}>
+          <li key={navItem.name} onClick={() => handleClick(navItem.name)}>
+            <a href={navItem.url} className={`${styles.navLink} ${activeSection === navItem.name && styles.active}`}>
               <hr />
               <span>{navItem.name}</span>
             </a>
